7018299ad6eae362eb719eefd6a91db96e0e4c90
Karina Hernandez Gamino <k.hernandex.g@gmail.com>
2025-12-05 01:00:48 -0800

    Last lab of the class

--- karina13_5.m
+++ karina13_5.m
@@ -4,6 +4,6 @@ time = 0:10;
 vel = [0 2 5 7 9 12 15 18 22 20 17];
 dist = trapz(time,vel);
 pos = dist + 3;
-% Report 
+% Report the results
 fprintf('The object travels a distance of %.1f , during the time.\n',dist)
 fprintf('So, the final position will be %.1f m.\n',pos)
